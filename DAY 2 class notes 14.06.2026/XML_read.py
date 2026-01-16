41efb0a99147b5267a418b4d8541f00bb2215203
Harsh Kumar Sinha <165485131+HarshSinha1406@users.noreply.github.com>
2026-01-16 09:24:52 +0530

    Delete DAY 2 class notes 14.06.2026 directory

--- DAY 2 class notes 14.06.2026/XML_read.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-tree=ET.parse("student.xml")
-root=tree.getroot()
-
-for student in root.findall("student"):
-    id=student.find("id").text
-    name = student.find("name").text
-    marks = student.find("marks").text
-    print(id,name,marks)
--- DAY 2 class notes 14.06.2026/filejson.py
+++
@@ -1,9 +0,0 @@
-import json
-data={
-    "name":"harsh",
-    "age":23,
-    "location":"patna , Bihar",
-    "skills":["python","java"],
-}
-with open("data.json","w") as file:
-    json.dump(data,file,indent=5)
