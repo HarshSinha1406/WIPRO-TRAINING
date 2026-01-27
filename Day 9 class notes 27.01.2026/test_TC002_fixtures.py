c7b1ed3da259eb2e3b96e8e87ae7e83cb94a6476
Harsh Kumar Sinha <165485131+HarshSinha1406@users.noreply.github.com>
2026-01-27 18:03:15 +0530

    Add files via upload

---
+++ Day 9 class notes 27.01.2026/test_TC002_fixtures.py
@@ -0,0 +1,7 @@
+def test_one():
+    data = [1 , 2 , 3]
+    assert 2 in data
+
+def test_two():
+    data = [1 , 2 , 3]
+    assert len(data) == 3
---
+++ Day 9 class notes 27.01.2026/test_TC004_yield.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.fixture()
+def setup_teardown():
+    print("setup")
+    yield
+    print("Teardown")
+
+
+def test_example(setup_teardown):
+    print("test running")
+def test_example1(setup_teardown):
+    print("testn2 running")
+
