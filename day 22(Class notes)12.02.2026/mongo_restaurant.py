5fb6dad53e9cce1a81c6cee7ca8c3b68fb7479db
Harsh Kumar Sinha <165485131+HarshSinha1406@users.noreply.github.com>
2026-02-12 18:19:12 +0530

    Update mongo_restaurant.py

--- day 22(Class notes)12.02.2026/mongo_restaurant.py
+++ day 22(Class notes)12.02.2026/mongo_restaurant.py
@@ -1,15 +1,11 @@
 from pymongo import MongoClient
 
-# Step 1: Connect to MongoDB
 client = MongoClient("mongodb://localhost:27017/")
 
-# Step 2: Connect to your database
 db = client["restaurant_db"]
 
-# Step 3: Connect to your collection
 collection = db["restaurants"]
 
-# Step 4: Create restaurant document (Teacher's data)
 new_restaurant = {
     "name": "Spice Villa",
     "location": "Bhubaneswar",
@@ -17,11 +13,10 @@ new_restaurant = {
     "rating": 4.5
 }
 
-# Step 5: Insert into MongoDB
 insert_result = collection.insert_one(new_restaurant)
 
-# Step 6: Print inserted data
 print("Inserted Restaurant ID:", insert_result.inserted_id)
 
 print("Inserted Document:")
 print(collection.find_one({"_id": insert_result.inserted_id}))
+
